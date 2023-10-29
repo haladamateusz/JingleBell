12d16383332efb6bb4032c761a490b84f21427ec
Mateusz Halada <haladamateusz@gmail.com>
2023-10-29 13:53:23 +0100

    add synthezis display

--- frontend/src/app/service/llama/llama.service.ts
+++ frontend/src/app/service/llama/llama.service.ts
@@ -15,30 +15,31 @@ export class LlamaService {
   private lastAnswer = new BehaviorSubject('');
 
   askQuestion(question: string, userId: string): Observable<LlamaResponseResult> {
-    return of({ ...LlamaResponseMock, question, userId }).pipe(
-      tap(data => {
-        this.lastResults.next(data);
-        this.lastQuestionAsked.next(question);
-        this.lastAnswer.next(data.answer);
-      })
-    );
-    // return this.httpClient
-    //   .post<LlamaResponseResult>('http://127.0.0.1:8000/askQuestion/', { question, userId })
-    //   .pipe(
-    //     tap(data => {
-    //       this.lastResults.next(data);
-    //       this.lastQuestionAsked.next(question);
-    //     })
-    //   );
+    // return of({ ...LlamaResponseMock, question, userId }).pipe(
+    //   tap(data => {
+    //     this.lastResults.next(data);
+    //     this.lastQuestionAsked.next(question);
+    //     this.lastAnswer.next(data.answer);
+    //   })
+    // );
+    return this.httpClient
+      .post<LlamaResponseResult>('http://127.0.0.1:8000/askQuestion/', { question, userId })
+      .pipe(
+        tap(data => {
+          this.lastResults.next(data);
+          this.lastQuestionAsked.next(question);
+        })
+      );
   }
 
   sendFeedback(feedback: FEEDBACK, userId: string): Observable<boolean> {
-    return of(true);
-    // return this.httpClient.post<boolean>('http://127.0.0.1:8000/sendFeedback/', {
-    //   feedback,
-    //   userId,
-    //   previousQuestion: this.lastQuestionAsked.getValue(),
-    //   previousResults: this.lastResults.getValue()
-    // });
+    // return of(true);
+    return this.httpClient.post<boolean>('http://127.0.0.1:8000/sendFeedback/', {
+      feedback,
+      userId,
+      previousQuestion: this.lastQuestionAsked.getValue(),
+      previousResults: this.lastResults.getValue(),
+      previousAnswer: this.lastAnswer.getValue()
+    });
   }
 }
